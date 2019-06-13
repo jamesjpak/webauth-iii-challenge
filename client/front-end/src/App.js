ad5e6034f8f53bf7b88aadf74664e039011da36d
James Pak <jamespak@Josephs-MacBook-Air.local>
2019-06-12 23:47:31 -0700

    SignUp comp scaffolding set up

--- client/front-end/src/App.js
+++ client/front-end/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import SignUp from './components/SignUp/SignUp';
+
 function App() {
+
   return (
+    <Router>
     <div className="App">
-      <h1> Baddabing Baddaboom! </h1>
+
+      <Route exact path="/" component={SignUp} />
+
     </div>
+    </Router>
   );
 }
 
---
+++ client/front-end/src/components/SignUp/SignUp.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import './SignUp.css';
+
+import { connect } from 'react-redux';
+// import { signUp } from '../../actions';
+
+class SignUp extends React.Component {
+    state = {
+        user: {
+            username: "",
+            password: "",
+            department: "",
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <h1> Sign-up to Login!</h1>
+
+                    <p> Username</p>
+                    <input 
+                    name='username' 
+                    required
+                    value={this.state.user.username}
+                    />
+
+                    <p> Password </p>
+                    <input
+                    name='password'
+                    required
+                    value={this.state.user.password}
+                    />
+
+                    <p> Department </p>
+                    <input
+                    name='department'
+                    required
+                    value={this.state.user.department}
+                    />
+            
+                </form>
+            </div>
+        )
+    }
+}
+
+export default connect(
+    // { signUp }
+)(SignUp)
