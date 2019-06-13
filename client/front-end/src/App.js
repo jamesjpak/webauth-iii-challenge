6eac00dc79ce91af18852f7f6ebc933ed0f3b9ef
James Pak <jamespak@Josephs-MacBook-Air.local>
2019-06-12 23:29:28 -0700

    dependencies installed, setting up sign-up comp

---
+++ client/front-end/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Baddabing Baddaboom! </h1>
+    </div>
+  );
+}
+
+export default App;
